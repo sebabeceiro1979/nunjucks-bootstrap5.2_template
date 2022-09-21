8e0684503e7ea2cc5da70597f505f6e7a119c135
Sebastián Beceiro <sebabeceiro@gmail.com>
2022-09-21 19:04:50 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 const app = express(); // Crea un instancia de Express.
 const routes = require("./routes");
-const nunjucks = require("nunjucks");
+const host = ("http://localhost")
+const PORT = process.env.APP_PORT || 5501;
 
-const PORT = process.env.APP_PORT || 5500;
+app.use(express.static(__dirname + '/views'));
+app.set("view engine", "njk");
 
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
 });
 
-app.set("view engine", "njk");
-
+app.use(express.static('node_modules/bootstrap/dist/'));
 app.use(routes);
 
-
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}!`));
+app.listen(PORT, () => console.log(`Servidor corriendo en ${host}:${PORT}!`));
